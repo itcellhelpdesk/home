a4974ef9af2880eb2b2ab0580adc80786e09eb91
itcellhelpdesk <corehelptvm@gmail.com>
2025-11-14 15:26:37 +0530

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,17 +1,19 @@
 const CACHE_NAME = 'app-cache-v1';
 const urlsToCache = [
-  '/home/',
   '/home/app/',
   '/home/app/index.html',
   '/home/app/style.css',
   '/home/app/script.js',
-  '/home/app/icons/icon-192x192.png'
+  '/home/app/manifest.json',
+  '/home/app/icons/icon-192x192.png',
+  '/home/app/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
